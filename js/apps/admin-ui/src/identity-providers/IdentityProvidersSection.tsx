39c62c345b4a4d5737d990dbd012f594d27963b2
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-05-13 08:30:33 +0200

    fixed test
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- js/apps/admin-ui/src/identity-providers/IdentityProvidersSection.tsx
+++ js/apps/admin-ui/src/identity-providers/IdentityProvidersSection.tsx
@@ -127,6 +127,7 @@ export default function IdentityProvidersSection() {
             key={provider.id}
             value={provider.id}
             component="a"
+            data-testid={provider.id}
             onClick={() =>
               navigate(
                 toIdentityProviderCreate({
